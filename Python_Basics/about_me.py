8abcf7b2df20c04ac7967760f56114638b598242
AJAYI ESTHER MIRACLE <estherajayi576@gmail.com>
2026-07-09 06:37:12 -0700

    Added about me Python program

---
+++ Python_Basics/about_me.py
@@ -0,0 +1,9 @@
+name = "Ajayi Esther"
+field = "Electrical and Electronic Engineering"
+goal = "AI and Robotics Engineer"
+country = "Nigeria"
+
+print("My name is:", name)
+print("I am studying:", field)
+print("My future goal is to become an:", goal)
+print("I am building my skills from:", country)
